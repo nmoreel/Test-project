2d16c5359dbb9948f301d15c13f828a82ad17d05
Nicolas MOREEL <nicolas1.moreel-lebon@epitech.eu>
2019-04-09 09:45:50 +0200

    add test jobs to travis and edit tests

--- tests/src/test-putstr.c
+++ tests/src/test-putstr.c
@@ -5,7 +5,7 @@
 ** Login   <nicolas1.moreel-lebon@epitech.eu>
 **
 ** Started on  Thu Jun 21 15:56:18 2018 Nicolas MOREEL
-** Last update Thu Jun 21 15:56:18 2018 Nicolas MOREEL
+
 */
 
 #include <criterion/criterion.h>
@@ -14,5 +14,5 @@ int     my_putstr(const char*);
 
 Test(putstr, first_test)
 {
-    cr_assert(my_putstr("Hello World\n") == 14);
+    cr_assert(my_putstr("Hello World\n") == 12);
 }
